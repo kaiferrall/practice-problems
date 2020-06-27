7015a47a57a31322c35cbb5a65e8347d2223760f
Kai Ferrall <33009041+kaiferrall@users.noreply.github.com>
2020-06-27 12:04:02 -0400

    Update 0625-62.py

--- 0625-62.py
+++ 0625-62.py
@@ -13,21 +13,25 @@ class Solution:
         if m == 1 or n == 1:
             return 1
         
-        if str((m, n - 1)) in mem:
-            r1 = mem[str((m, n - 1))]
+        if (m, n - 1) in mem:
+            r1 = mem[(m, n - 1)]
         
-        if str((m - 1, n)) in mem:
-            r2 = mem[str((m - 1, n))]
+        if (m - 1, n) in mem:
+            r2 = mem[(m - 1, n)]
         
 
         if r1 and r2:
             res = r1 + r2
         elif r1 and not r2:
-            res = self.helper(m - 1, n, mem) + r1
+            res = self.find_paths(m - 1, n, mem) + r1
         elif r2 and not r1:
-            res = self.helper(m, n - 1, mem) + r2
+            res = self.find_paths(m, n - 1, mem) + r2
         else:
-            res = self.helper(m - 1, n, mem) + self.helper(m, n - 1, mem)
+            res = self.find_paths(m - 1, n, mem) + self.find_paths(m, n - 1, mem)
 
-        mem[str((m, n))] = res
+        mem[(m, n)] = res
         return res
+        
+        
+        
+        
